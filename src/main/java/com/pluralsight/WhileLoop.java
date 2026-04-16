2fd0456b07a44c139ee955219241b8aa70de0e4d
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-16 12:17:28 -0400

    Changed while loop so it only prints out 5 times and commented.

--- src/main/java/com/pluralsight/WhileLoop.java
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -1,4 +1,13 @@
 package com.pluralsight;
 
 public class WhileLoop {
+    public static void main(String[] args) {
+        //While Loop To Print I love Java 5 Times
+        int i = 0;
+        while (i < 5){
+            System.out.println("I love Java!");
+            i++;
+        }
+
+    }
 }
