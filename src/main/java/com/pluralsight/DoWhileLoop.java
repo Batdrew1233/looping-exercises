5dcd9948ceb5f783179a4c2a87eb8f599bab356a
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-16 12:21:21 -0400

    Made a Do/while class and loop to print I love Java 5 times.

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        //Do/While to print I love Java 5 times
+        int i = 0;
+        do{
+            System.out.println("I love Java");
+            i++;
+
+        }while(i<5);
+    }
+}
