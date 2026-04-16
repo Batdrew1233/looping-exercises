b3a240610b112c228a8bd6e3076d3ebae1e19066
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-16 12:42:40 -0400

    Added Thread.sleep to my For Loop to add time between the outputs.

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -1,11 +1,13 @@
 package com.pluralsight;
 
 public class ForLoop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //Using for to count down from 10 and print launch after it hits 1
         int i;
         for (i = 10; i >= 1; i--){
             System.out.println(i + "!");
+            Thread.sleep(1000);
+
         }
         System.out.println("Launch!");
     }
